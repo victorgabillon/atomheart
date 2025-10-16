ed25bc0cb5a3524939b065fd3e02f59ff05fecd9
Victor Gabillon <victor.gabillon@thalesgroup.com>
2025-10-16 17:38:11 +0200

    exposing more

--- src/atomheart/__init__.py
+++ src/atomheart/__init__.py
@@ -3,6 +3,6 @@ init file for chess environment
 """
 
 from .board.board_chi import BoardChi
-from .board.factory import create_board_chi
+from .board.factory import create_board_chi, create_board_chi_from_pychess_board
 
-__all__ = ["BoardChi", "create_board_chi"]
+__all__ = ["BoardChi", "create_board_chi", "create_board_chi_from_pychess_board"]
