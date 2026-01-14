ff66b83643d4d1a32f43a07eaff3f4aadff9b5df
victorgabillon <victorgabillon@gmail.com>
2026-01-14 12:51:28 +0000

    HalfMove

--- src/atomheart/move/utils.py
+++ src/atomheart/move/utils.py
@@ -5,3 +5,5 @@ Utilities for move UCI representation.
 from typing import Annotated, TypeAlias
 
 MoveUci: TypeAlias = Annotated[str, "a string representing a move uci"]
+
+HalfMove: TypeAlias = Annotated[int, "an integer representing a half move"]
